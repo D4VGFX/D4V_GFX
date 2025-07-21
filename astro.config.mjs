07eb512c9ef989d8318363bf3e135297d8cd4d01
D4VGFX <d4v.gfx@gmail.com>
2025-07-21 18:41:36 +0200

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://d4vgfx.com',
-  base: '/',
-  outDir: './dist',
-  build: {
-    format: 'directory',
-  }
+  output: 'static',
 });
